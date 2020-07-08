c9eed22dd5f8087d686cbc8fafafc05e3fca4cd3
mohaned-mashaly <mohaned_boss@outlook.com>
2020-07-08 12:13:16 +0200

    non-stop words filter

--- Channel/views.py
+++ Channel/views.py
@@ -18,12 +18,11 @@ def Home(request):
     if request.method == "POST":
         Body = request.POST.get("MessageForm")
         Body_Tokenized = word_tokenize(Body)
-        modifiedBody_Tokenized = []
-        for word in Body_Tokenized:
-            if not word in stop_words:
-                modifiedBody_Tokenized.append(word)
+        modifiedBody_Tokenized = remove_stopwords(Body_Tokenized)
+        #print(nltk.pos_tag(modifiedBody_Tokenized))
     return render(request,template_name = "Channel/Home.html")
 
+#returning the words to it's origin
 def word_stemming(Sentence):
     Stemmer = PorterStemmer()
     Stemmed = []
@@ -31,3 +30,49 @@ def word_stemming(Sentence):
         if not word in stop_words:
             Stemmed.append(Stemmer.stem(word))
     return Stemmed
+
+#remove stop words from the speech
+def remove_stopwords(Text):
+    Modified_Body = []
+    for word in Text:
+        if not word in stop_words:
+            Modified_Body.append(word)
+    return Modified_Body
+"""
+POS tag list:
+
+CC	coordinating conjunction
+CD	cardinal digit
+DT	determiner
+EX	existential there (like: "there is" ... think of it like "there exists")
+FW	foreign word
+IN	preposition/subordinating conjunction
+JJ	adjective	'big'
+JJR	adjective, comparative	'bigger'
+JJS	adjective, superlative	'biggest'
+LS	list marker	1)
+MD	modal	could, will
+NN	noun, singular 'desk'
+NNS	noun plural	'desks'
+NNP	proper noun, singular	'Harrison'
+NNPS	proper noun, plural	'Americans'
+PDT	predeterminer	'all the kids'
+POS	possessive ending	parent\'s
+PRP	personal pronoun	I, he, she
+PRP$	possessive pronoun	my, his, hers
+RB	adverb	very, silently,
+RBR	adverb, comparative	better
+RBS	adverb, superlative	best
+RP	particle	give up
+TO	to	go 'to' the store.
+UH	interjection	errrrrrrrm
+VB	verb, base form	take
+VBD	verb, past tense	took
+VBG	verb, gerund/present participle	taking
+VBN	verb, past participle	taken
+VBP	verb, sing. present, non-3d	take
+VBZ	verb, 3rd person sing. present	takes
+WDT	wh-determiner	which
+WP	wh-pronoun	who, what
+WP$	possessive wh-pronoun	whose
+WRB	wh-abverb	where, when"""
